1fba99f784685d216080810c62209f69a0aa356c
Amos Tang <amostang1994@gmail.com>
2021-03-01 22:27:17 +0800

    Create Hackxercise_7-5.py

---
+++ Hackxercise_7-5.py
@@ -0,0 +1,25 @@
+# Hackxercise_7-5
+
+# Real RSA system:
+# Use pycrypto to sign and verify a message using RSA.
+
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+
+private_key = key.exportKey('PEM')
+public_key = key.publickey().exportKey('PEM')
+
+def sign(m, private_key):
+    return key.decrypt(m)
+
+def verify(m, s, public_key):
+    if key.encrypt(s, public_key) == m:
+        return 1
+    else:
+        return 0
+        
+#  test
+print(sign(3, private_key))
+
+print(verify(3, sign(3, private_key), public_key))
