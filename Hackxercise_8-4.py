e58adf51185a69a7b8b47a01710a6f101eb7a483
Amos Tang <amostang1994@gmail.com>
2021-03-02 22:14:20 +0800

    Create Hackxercise_8-4.py

---
+++ Hackxercise_8-4.py
@@ -0,0 +1,17 @@
+# Hackxercise_8-4
+
+# Patch the following code so it's no longer susceptible to SQL injections:
+
+import sqlite3
+
+with sqlite3.connect('db.sqlite3') as connection:
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
+    cursor.execute('INSERT OR IGNORE INTO users VALUES (1, "alice", "1234")')
+    cursor.execute('INSERT OR IGNORE INTO users VALUES (2, "bob", "5678")')
+
+def authenticate(username, password):
+    with sqlite3.connect('db.sqlite3') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password)) # change this line only
+        return len(cursor.fetchall()) > 0
